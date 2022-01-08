a6bec085f3cb4e945f02828ea18de96d93eaba4a
irev <refyandra@gmail.com>
2022-01-08 20:46:02 +0700

    ok

--- app.js
+++ app.js
@@ -238,10 +238,13 @@ app.use(function (req, res, next) {
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  
+  //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Vary: Access-Control-Request-Headers');
   res.setHeader('Content-Type', 'application/json');
   res.setHeader('Access-Control-Max-Age', '1000');
   // Pass to next layer of middleware
