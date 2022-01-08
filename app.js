500723a552eaacb5e989bcc8e3296d0c86affc89
Andra Meedun <irev@users.noreply.github.com>
2022-01-08 14:47:17 +0700

    Access-Control-Allow-Origin': '*'
    add
    ```
    
     res.set({
        'Access-Control-Allow-Origin': '*',
        'Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
        'Access-Control-Allow-Headers', 'X-Requested-With,content-type',
        'Access-Control-Allow-Credentials', true,
        'Content-Type': 'application/json',
      });
    
    ```

--- app.js
+++ app.js
@@ -24,30 +24,6 @@ app.use(fileUpload({
   debug: true
 }));
 
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-  
-  res.setHeader('Content-Type', 'application/json');
-
-    // Pass to next layer of middleware
-    next();
-});
-
-
-
 
 const SESSION_FILE_PATH = './whatsapp-session.json';
 let sessionCfg;
@@ -234,8 +210,39 @@ app.post('/send-message', [
   });
 });
 
+
+
+// // Add headers before the routes are defined
+// app.use(function (req, res, next) {
+
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+  
+//   res.setHeader('Content-Type', 'application/json');
+
+//     // Pass to next layer of middleware
+//     next();
+// });
+
 // Send media
 app.post('/send-media', async (req, res) => {
+  res.set({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+    'Access-Control-Allow-Headers', 'X-Requested-With,content-type',
+    'Access-Control-Allow-Credentials', true,
+    'Content-Type': 'application/json',
+  });
   const number = phoneNumberFormatter(req.body.number);
   const caption = req.body.caption;
   const fileUrl = req.body.file;
